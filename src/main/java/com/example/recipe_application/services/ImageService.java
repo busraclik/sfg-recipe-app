c56aa6c1d226b4ac405e5d057b188a62c79e2fb8
busraclik <rabusracelik@gmail.com>
2024-12-26 09:16:59 +0300

    some name refactoring

--- src/main/java/com/example/recipe_application/services/ImageService.java
+++ src/main/java/com/example/recipe_application/services/ImageService.java
@@ -3,5 +3,5 @@ package com.example.recipe_application.services;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
-    void saveImageFile(Long imageId, MultipartFile file);
+    void saveImageFile(Long recipeId, MultipartFile file);
 }
